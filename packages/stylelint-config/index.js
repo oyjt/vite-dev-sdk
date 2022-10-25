dde6c83b1399ec81306531ea36424342482e3a6d
oyjt <oyjt001@gmail.com>
2022-10-25 11:12:54 +0800

    框架优化

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+		plugins: ['stylelint-order'],
     rules: {
       'no-descending-specificity': null,
     },
   }
-  
